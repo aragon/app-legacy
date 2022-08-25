d9482b8fa114f4a84bf9f4b4f7e88467ecf560d3
sepehr sanaee <sepehr2mail@gmail.com>
2022-08-25 17:24:20 +0430

    removed useDao hook and add usePluginClient hook

--- packages/web-app/src/hooks/usePluginClient.tsx
+++ packages/web-app/src/hooks/usePluginClient.tsx
@@ -12,6 +12,8 @@ interface IUseDaoResponse {
   createWhitelist: (pluginAddress: Address) => ClientAddressList | Error;
 }
 
+type ClientTypes = 'ERC20' | 'Whitelist';
+
 /**
  * This hook can be used to build ERC20 or whitelist clients
  * @method createErc20 By passing instance plugin address will create an ERC20Client
@@ -19,29 +21,25 @@ interface IUseDaoResponse {
  * @returns The corresponding Client
  */
 
-export const useDao = (): IUseDaoResponse => {
+export const usePluginClient = (
+  type: ClientTypes,
+  pluginAddress: Address
+): ClientErc20 | ClientAddressList | Error => {
   const {client, context} = useClient();
 
-  const createErc20 = (pluginAddress: Address): ClientErc20 | Error => {
-    if (!client || !context) {
-      return new Error('ERC20 SDK client is not initialized correctly');
-    }
+  if (!client || !context) {
+    return new Error('SDK client is not initialized correctly');
+  }
+
+  if (type === 'ERC20') {
     const contextPlugin: ContextPlugin = ContextPlugin.fromContext(
       context as Context,
       pluginAddress
     );
-
     const clientERC20: ClientErc20 = new ClientErc20(contextPlugin);
 
     return clientERC20;
-  };
-
-  const createWhitelist = (
-    pluginAddress: Address
-  ): ClientAddressList | Error => {
-    if (!client || !context) {
-      return new Error('Whitelist SDK client is not initialized correctly');
-    }
+  } else if (type === 'Whitelist') {
     const contextPlugin: ContextPlugin = ContextPlugin.fromContext(
       context as Context,
       pluginAddress
@@ -52,9 +50,7 @@ export const useDao = (): IUseDaoResponse => {
     );
 
     return createWhitelist;
-  };
-  return {
-    createWhitelist,
-    createErc20,
-  };
+  } else {
+    return new Error('The requested sdk type is invalid');
+  }
 };
