c69bc073322f94edaaae1862416e9bf92861b887
Fabrice Francois <fabricevladimir@gmail.com>
2022-11-09 12:26:57 -0500

    stateEmpty initial refactor

--- packages/ui-components/src/components/stateEmpty/stateEmpty.tsx
+++ packages/ui-components/src/components/stateEmpty/stateEmpty.tsx
@@ -38,7 +38,7 @@ export type StateEmptyProps =
 
 export const StateEmpty: React.FC<StateEmptyProps> = props => {
   return (
-    <Card mode={props.mode} size={props.size || 'small'}>
+    <Card mode={props.mode} size={props.size || 'small'} type={props.type}>
       {props.type === 'Human' ? (
         <IllustrationHuman
           {...{
@@ -54,65 +54,98 @@ export const StateEmpty: React.FC<StateEmptyProps> = props => {
       ) : (
         <IlluObject object={props.object} />
       )}
-      <TextWrapper>
-        <Title>{props.title}</Title>
-        {props.renderHtml ? (
-          <Description
-            dangerouslySetInnerHTML={{__html: props.description || ''}}
-          />
-        ) : (
-          props.description && <Description>{props.description}</Description>
+      <ContentWrapper>
+        <TextWrapper>
+          <Title>{props.title}</Title>
+          {props.renderHtml ? (
+            <Description
+              size={props.size}
+              dangerouslySetInnerHTML={{__html: props.description || ''}}
+            />
+          ) : (
+            props.description && (
+              <Description size={props.size}>{props.description}</Description>
+            )
+          )}
+        </TextWrapper>
+        {(props.primaryButton || props.secondaryButton) && (
+          <ActionContainer size={props.size || 'small'}>
+            {props.primaryButton && (
+              <ButtonText
+                label={props.primaryButton.label}
+                onClick={props.primaryButton.onClick}
+                iconLeft={props.primaryButton.iconLeft}
+                iconRight={props.primaryButton.iconRight}
+                size="large"
+              />
+            )}
+            {props.secondaryButton && (
+              <ButtonText
+                label={props.secondaryButton.label}
+                onClick={props.secondaryButton.onClick}
+                iconLeft={props.secondaryButton.iconLeft}
+                iconRight={props.secondaryButton.iconRight}
+                mode="secondary"
+                size="large"
+                bgWhite
+              />
+            )}
+          </ActionContainer>
         )}
-      </TextWrapper>
-      {(props.primaryButton || props.secondaryButton) && (
-        <ActionContainer>
-          {props.primaryButton && (
-            <ButtonText
-              label={props.primaryButton.label}
-              onClick={props.primaryButton.onClick}
-              iconLeft={props.primaryButton.iconLeft}
-              iconRight={props.primaryButton.iconRight}
-              size="large"
-            />
-          )}
-          {props.secondaryButton && (
-            <ButtonText
-              label={props.secondaryButton.label}
-              onClick={props.secondaryButton.onClick}
-              iconLeft={props.secondaryButton.iconLeft}
-              iconRight={props.secondaryButton.iconRight}
-              mode="ghost"
-              size="large"
-            />
-          )}
-        </ActionContainer>
-      )}
+      </ContentWrapper>
     </Card>
   );
 };
 
-const Card = styled.div.attrs<Pick<Props, 'mode' | 'size'>>(({mode, size}) => ({
-  className: `flex flex-col items-center justify-center rounded-xl w-full space-y-3 ${
-    mode === 'card' ? 'border bg-ui-0' : 'bg-ui-0'
-  } ${size === 'large' ? 'p-6' : 'p-3'}`,
-}))<Pick<Props, 'mode' | 'size'>>``;
+const Card = styled.div.attrs<Pick<StateEmptyProps, 'mode' | 'size' | 'type'>>(
+  ({mode, size, type}) => {
+    let className = 'flex flex-col items-center rounded-xl w-full ';
+
+    if (mode === 'card') {
+      className += 'border bg-ui-0 ';
+
+      className += size === 'small' ? 'p-3 ' : (className += 'p-6 ');
+      if (type === 'Object') className += 'gap-y-1 ';
+    } else {
+      className += 'bg-ui-transparent ';
+    }
+
+    if (type === 'Human') className += 'gap-y-3 ';
+    return {className};
+  }
+)<Pick<StateEmptyProps, 'mode' | 'size' | 'type'>>``;
+
+const ContentWrapper = styled.div.attrs({className: 'space-y-3 w-full'})``;
 
 const TextWrapper = styled.div.attrs({
-  className: 'space-y-2 text-center',
+  className: 'space-y-1.5 text-center',
 })``;
 
-const ActionContainer = styled.div.attrs({
-  className:
-    'flex desktop:flex-row flex-col space-y-1.5 desktop:space-y-0 space-x-0 desktop:space-x-3',
-})``;
+const ActionContainer = styled.div.attrs<Pick<StateEmptyProps, 'size'>>(
+  ({size}) => {
+    let className = 'flex w-full ';
+
+    if (size === 'small') {
+      className += 'flex-col gap-y-1.5';
+    } else {
+      className += 'justify-center gap-x-3';
+    }
+
+    return {className};
+  }
+)<Pick<StateEmptyProps, 'size'>>``;
 
 const Title = styled.h2.attrs({
   className: 'ft-text-xl font-bold text-ui-800',
 })``;
 
-const Description = styled.p.attrs({
-  className: 'text-ui-500 text-sm desktop:text-base',
-})`
+const Description = styled.p.attrs<Pick<StateEmptyProps, 'size'>>(({size}) => {
+  return {
+    className: `text-ui-500 ${
+      size === 'small' ? 'ft-text-sm' : 'ft-text-base'
+    }`,
+  };
+})<Pick<StateEmptyProps, 'size'>>`
   & > a {
     color: #003bf5;
     font-weight: 700;
