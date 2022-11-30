cb3439652c23cacc2fe18b2a79fa2c7d306970b2
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2022-11-30 18:40:21 +0100

    narrowing type

--- packages/web-app/src/context/apolloClient.tsx
+++ packages/web-app/src/context/apolloClient.tsx
@@ -156,8 +156,7 @@ const pendingProposals = JSON.parse(
 const pendingProposalsVar = makeVar<PendingProposals>(pendingProposals);
 
 type PendingDaoCreation = {
-  // This key is the network in string representation
-  [key: string]: {
+  [key in SupportedNetworks]?: {
     // This key is the id of the newly created DAO
     [key: string]: ICreateParams;
   };
