dc12dc5a113a63655275e63d5294c6573f167267
Fabrice Francois <fabricevladimir@gmail.com>
2022-12-07 08:37:01 -0500

    wip

--- packages/web-app/src/components/protectedRoute/index.tsx
+++ packages/web-app/src/components/protectedRoute/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect} from 'react';
 import {Outlet} from 'react-router-dom';
 
 import {Loading} from 'components/temporary';
@@ -9,6 +9,10 @@ import {useDaoMembers} from 'hooks/useDaoMembers';
 import {useDaoParam} from 'hooks/useDaoParam';
 import {PluginTypes} from 'hooks/usePluginClient';
 import {useWallet} from 'hooks/useWallet';
+import {fetchBalance} from 'utils/tokens';
+import {CHAIN_METADATA} from 'utils/constants';
+import {useSpecificProvider} from 'context/providers';
+import {useNetwork} from 'context/network';
 
 const ProtectedRoute: React.FC = () => {
   const {data: dao, isLoading: paramIsLoading} = useDaoParam();
@@ -26,6 +30,26 @@ const ProtectedRoute: React.FC = () => {
     address as string
   );
 
+  const {network} = useNetwork();
+  const provider = useSpecificProvider(CHAIN_METADATA[network].id);
+
+  const checkIfTokenbasedMember = useCallback(async () => {
+    if (daoToken && address) {
+      const balance = await fetchBalance(
+        daoToken?.address,
+        address,
+        provider,
+        CHAIN_METADATA[network].nativeCurrency
+      );
+
+      if (Number(balance) === 0) open('gating');
+    }
+  }, [address, daoToken, network, open, provider]);
+
+  const checkIfAllowlistedMember = useCallback(() => {
+    if (filteredMembers.length === 0) open('gating');
+  }, [filteredMembers.length, open]);
+
   useEffect(() => {
     // Note: if user came to protected routes by direct link the status could be disconnected > connecting > connected
     // In this scenario "close" on else case will help to fix unexpected behaviors at the wallet loading moment
@@ -36,23 +60,26 @@ const ProtectedRoute: React.FC = () => {
       else close('network');
     }
 
-    if (
-      filteredMembers.length === 0 &&
-      daoDetails &&
-      isConnected &&
-      !isOnWrongNetwork
-    ) {
-      open('gating');
-    }
-  }, [
-    close,
-    daoDetails,
-    filteredMembers.length,
-    isConnected,
-    isOnWrongNetwork,
-    open,
-    status,
-  ]);
+    // if (
+    //   daoDetails &&
+    //   isConnected &&
+    //   !isOnWrongNetwork &&
+    //   filteredMembers.length === 0
+    // ) {
+    //   if (daoDetails.plugins[0].id === 'erc20voting.dao.eth')
+    //     // only checking for members that got token directly not from dao
+    //     checkIfTokenbasedMember();
+    //   else open('gating');
+    // }
+    // if (
+    //   filteredMembers.length === 0 &&
+    //   daoDetails &&
+    //   isConnected &&
+    //   !isOnWrongNetwork
+    // ) {
+    //   open('gating');
+    // }
+  }, [close, isConnected, isOnWrongNetwork, open, status]);
 
   if (paramIsLoading || detailsAreLoading || MembershipIsLoading)
     return <Loading />;
@@ -60,11 +87,11 @@ const ProtectedRoute: React.FC = () => {
   return (
     <>
       <Outlet />
-      <GatingMenu
+      {/* <GatingMenu
         daoAddress={dao}
         pluginType={daoDetails?.plugins[0].id as PluginTypes}
         tokenName={daoToken?.name}
-      />
+      /> */}
     </>
   );
 };
