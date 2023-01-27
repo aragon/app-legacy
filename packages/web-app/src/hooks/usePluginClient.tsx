68098a6ce2d5821c582d4a1cdc9e60d5c7845f3f
Fabrice Francois <fabricevladimir@gmail.com>
2023-01-27 07:37:10 -0500

    generic plugin client

--- packages/web-app/src/hooks/usePluginClient.tsx
+++ packages/web-app/src/hooks/usePluginClient.tsx
@@ -11,49 +11,48 @@ export type PluginTypes =
   | 'token-voting.plugin.dao.eth'
   | 'multisig.plugin.dao.eth';
 
-type PluginClientTypes = MultisigClient | TokenVotingClient | undefined;
+type PluginType<T> = T extends 'token-voting.plugin.dao.eth'
+  ? TokenVotingClient
+  : T extends 'multisig.plugin.dao.eth'
+  ? MultisigClient
+  : never;
+
 /**
  * This hook can be used to build ERC20 or whitelist clients
  * @param pluginType Type of plugin for which a client is to be built. Note that
  * this is information that must be fetched. I.e., it might be unavailable on
  * first render. Therefore, it is typed as potentially undefined.
- * @method createErc20 By passing instance plugin address will create an
- * ERC20Client
- * @method createWhitelist By passing instance plugin address will create an
- * WhitelistClient
  * @returns The corresponding Client
  */
-export const usePluginClient = (
-  pluginType?: PluginTypes
-): PluginClientTypes => {
-  const [pluginClient, setPluginClient] =
-    useState<PluginClientTypes>(undefined);
+export const usePluginClient = <T extends PluginTypes = PluginTypes>(
+  pluginType?: T
+): PluginType<T> | undefined => {
+  const [pluginClient, setPluginClient] = useState<PluginType<PluginTypes>>();
+
   const {client, context} = useClient();
 
   useEffect(() => {
-    if (!client || !context) {
-      // throw new Error('SDK client is not initialized correctly');
-      return;
-    }
+    if (!client || !context) return;
 
-    if (!pluginType) setPluginClient(undefined);
-    else {
-      switch (pluginType) {
-        case 'token-voting.plugin.dao.eth':
-          setPluginClient(
-            new TokenVotingClient(ContextPlugin.fromContext(context))
-          );
-          break;
+    if (!pluginType) {
+      setPluginClient(undefined);
+    } else {
+      switch (pluginType as PluginTypes) {
         case 'multisig.plugin.dao.eth':
           setPluginClient(
             new MultisigClient(ContextPlugin.fromContext(context))
           );
           break;
+        case 'token-voting.plugin.dao.eth':
+          setPluginClient(
+            new TokenVotingClient(ContextPlugin.fromContext(context))
+          );
+          break;
         default:
-          throw new Error('The requested sdk type is invalid');
+          throw new Error('The requested plugin type is invalid');
       }
     }
   }, [client, context, pluginType]);
 
-  return pluginClient;
+  return pluginClient as PluginType<T>;
 };
