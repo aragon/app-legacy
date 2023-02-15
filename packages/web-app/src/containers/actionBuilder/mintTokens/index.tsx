66ea5f9c7bb29336b8762982c6d261da40857974
George <giorgi.bibilashvili89@gmail.com>
2023-02-15 14:42:08 +0400

    Fix tailwind classname order

--- packages/web-app/src/containers/actionBuilder/mintTokens/index.tsx
+++ packages/web-app/src/containers/actionBuilder/mintTokens/index.tsx
@@ -409,7 +409,7 @@ export const MintTokenForm: React.FC<MintTokenFormProps> = ({
           onClick={handleAddWallet}
         />
 
-        <label className="flex-1 tablet:flex-initial py-1.5 px-2 space-x-1.5 h-6 font-bold rounded-xl cursor-pointer hover:text-primary-500 bg-ui-0 ft-text-base">
+        <label className="flex-1 tablet:flex-initial py-1.5 px-2 space-x-1.5 h-6 font-bold hover:text-primary-500 bg-ui-0 rounded-xl cursor-pointer ft-text-base">
           {t('labels.whitelistWallets.uploadCSV')}
           <input
             type="file"
