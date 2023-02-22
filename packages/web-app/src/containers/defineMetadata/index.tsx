2d0b2f78da8536edcf4f06b4614b78ffd89a2e23
sepehr sanaee <sepehr2mail@gmail.com>
2023-02-22 12:15:46 +0330

    ens label added

--- packages/web-app/src/containers/defineMetadata/index.tsx
+++ packages/web-app/src/containers/defineMetadata/index.tsx
@@ -154,6 +154,11 @@ const DefineMetadata: React.FC<DefineMetadataProps> = ({
                 <TextInput
                   {...{name, value, onBlur, onChange}}
                   placeholder={t('placeHolders.ensName')}
+                  rightAdornment={
+                    <div className="flex items-center px-2 h-full bg-ui-50">
+                      .dao.eth
+                    </div>
+                  }
                 />
                 <InputCount>{`${value.length}/128`}</InputCount>
                 <ErrorHandler {...{value, error}} />
