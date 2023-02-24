5317e2526a61a3220c1def405faa1e574f77cdda
sepehr sanaee <sepehr2mail@gmail.com>
2023-02-24 16:51:37 +0330

    revert daoFactory address

--- packages/web-app/src/hooks/useClient.tsx
+++ packages/web-app/src/hooks/useClient.tsx
@@ -41,6 +41,7 @@ export const UseClientProvider: React.FC = ({children}) => {
           },
         },
       ],
+      daoFactoryAddress: '0xf401dbc7eEf9E8DE629E67154838e8a7D828D2A3',
       graphqlNodes: [
         {
           url: SUBGRAPH_API_URL[network]!,
