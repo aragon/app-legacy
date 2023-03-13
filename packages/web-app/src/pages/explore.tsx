82b612c90061b6ee8770f454a6f71e5621107998
James Ellis-Jones <jamesej@outlook.com>
2023-03-13 12:10:34 +0000

    remove commented code

--- packages/web-app/src/pages/explore.tsx
+++ packages/web-app/src/pages/explore.tsx
@@ -18,15 +18,6 @@ import {useWallet} from 'hooks/useWallet';
 const Explore: React.FC = () => {
   const {setNetwork} = useNetwork();
 
-  /* useNetwork should take this into account
-  const {chainId} = useWallet();
-
-  useEffect(() => {
-    const network = getSupportedNetworkByChainId(chainId) as SupportedNetworks;
-    if (['ethereum', 'goerli'].includes(network)) setNetwork(network);
-  }, [chainId, setNetwork]);
-  */
-
   return (
     <>
       <Hero />
