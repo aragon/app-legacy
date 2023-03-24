08057d1f46fa61fbed81da043e403cecab5dc8dc
RakeshUP <parthiban.rakesh95@gmail.com>
2023-03-24 15:35:25 +0530

    sanitize-html on proposal page

--- packages/web-app/src/pages/proposal.tsx
+++ packages/web-app/src/pages/proposal.tsx
@@ -27,6 +27,7 @@ import React, {useEffect, useMemo, useRef, useState} from 'react';
 import {useTranslation} from 'react-i18next';
 import {generatePath, useNavigate, useParams} from 'react-router-dom';
 import styled from 'styled-components';
+import sanitizeHtml from 'sanitize-html';
 
 import {ExecutionWidget} from 'components/executionWidget';
 import ResourceList from 'components/resourceList';
@@ -190,7 +191,12 @@ const Proposal: React.FC = () => {
   // set editor data
   useEffect(() => {
     if (proposal && editor) {
-      editor.commands.setContent(proposal.metadata.description, true);
+      editor.commands.setContent(
+        sanitizeHtml(proposal.metadata.description, {
+          disallowedTagsMode: 'recursiveEscape',
+        }),
+        true
+      );
     }
   }, [editor, proposal]);
 
