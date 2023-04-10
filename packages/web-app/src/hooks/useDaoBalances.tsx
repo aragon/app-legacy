3dd3727721595ee8e3cb9ee0e21c9fba09ae8e7b
sepehr sanaee <sepehr2mail@gmail.com>
2023-04-10 18:54:08 +0330

    add comments to useDaoBalances

--- packages/web-app/src/hooks/useDaoBalances.tsx
+++ packages/web-app/src/hooks/useDaoBalances.tsx
@@ -1,5 +1,5 @@
 import {AssetBalance, TokenType} from '@aragon/sdk-client';
-import {useEffect, useState} from 'react';
+import {useEffect, useMemo, useState} from 'react';
 import {alchemyApiKeys, CHAIN_METADATA} from 'utils/constants';
 
 import {HookData} from 'utils/types';
@@ -18,51 +18,48 @@ export const useDaoBalances = (
 
   const provider = useSpecificProvider(CHAIN_METADATA[network].id);
 
-  // ALCHEMY URL --> Replace with your API Key at the end
+  // Construct the Alchemy API URL
   const url = `${CHAIN_METADATA[network].alchemyApi}/${alchemyApiKeys[network]}`;
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const options = {
-    method: 'POST',
-    headers: {
-      accept: 'application/json',
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify({
-      id: 1,
-      jsonrpc: '2.0',
-      method: 'alchemy_getTokenBalances',
-      params: [daoAddressOrEns],
+  // Memoize the options object to prevent unnecessary re-renders
+  const options = useMemo(
+    () => ({
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: 1,
+        jsonrpc: '2.0',
+        method: 'alchemy_getTokenBalances',
+        params: [daoAddressOrEns],
+      }),
     }),
-  };
+    [daoAddressOrEns]
+  );
 
+  // Use the useEffect hook to fetch DAO balances
   useEffect(() => {
     async function getBalances() {
       try {
         setIsLoading(true);
 
+        // Fetch the token list using the Alchemy API
         const res = await fetch(url, options);
         const tokenList = await res.json();
         let nativeTokenBalances = [] as Array<AssetBalance>;
 
+        // Filter out tokens with a zero balance
         const nonZeroBalances = tokenList.result.tokenBalances.filter(
           (token: {tokenBalance: string}) => {
             return BigInt(token.tokenBalance) !== BigInt(0);
           }
         );
 
-        await provider.getBalance(daoAddressOrEns).then(balance => {
-          if (!balance.eq(0))
-            nativeTokenBalances = [
-              {
-                type: TokenType.NATIVE,
-                ...CHAIN_METADATA[network].nativeCurrency,
-                updateDate: new Date(),
-                balance: BigInt(balance.toString()),
-              },
-            ];
-        });
+        const fetchNativeCurrencyBalance = provider.getBalance(daoAddressOrEns);
 
+        // Define a list of promises to fetch ERC20 token balances
         const tokenListPromises = nonZeroBalances.map(
           async ({
             contractAddress,
@@ -88,7 +85,24 @@ export const useDaoBalances = (
           }
         );
 
-        const erc20balances = await Promise.all(tokenListPromises);
+        // Wait for both native currency and ERC20 balances to be fetched
+        const [nativeCurrencyBalance, erc20balances] = await Promise.all([
+          fetchNativeCurrencyBalance,
+          Promise.all(tokenListPromises),
+        ]);
+
+        // If the native currency balance is non-zero, add it to the list
+        if (!nativeCurrencyBalance.eq(0)) {
+          nativeTokenBalances = [
+            {
+              type: TokenType.NATIVE,
+              ...CHAIN_METADATA[network].nativeCurrency,
+              updateDate: new Date(),
+              balance: BigInt(nativeCurrencyBalance.toString()),
+            },
+          ];
+        }
+
         if (erc20balances) setData([...nativeTokenBalances, ...erc20balances]);
       } catch (error) {
         console.error(error);
@@ -99,7 +113,7 @@ export const useDaoBalances = (
     }
 
     getBalances();
-  }, []);
+  }, [daoAddressOrEns, network, options, provider, url]);
 
   return {data, error, isLoading};
 };
