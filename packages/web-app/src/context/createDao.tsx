119db261962cd86df5a92854126eb60c6bcacd11
Barukimang <barukimang@gmail.com>
2023-04-20 12:39:35 +0300

    edit default minProposerVotingPower to 1 token times 10 to the power of 18

--- packages/web-app/src/context/createDao.tsx
+++ packages/web-app/src/context/createDao.tsx
@@ -201,7 +201,7 @@ const CreateDaoProvider: React.FC = ({children}) => {
         minProposerVotingPower:
           eligibilityType === 'token' && eligibilityTokenAmount !== undefined
             ? parseUnits(eligibilityTokenAmount.toString(), 18).toBigInt()
-            : BigInt(0),
+            : BigInt(1000000000000000000),
         votingMode,
       },
       translatedNetwork,
