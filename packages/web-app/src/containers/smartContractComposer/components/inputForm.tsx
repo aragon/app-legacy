4ef34519edaa337f29406116a0f878874e19e1fc
babarella <nikitooos.golubev@gmail.com>
2023-06-15 12:01:26 -0300

    refactor: PR feedback fix

--- packages/web-app/src/containers/smartContractComposer/components/inputForm.tsx
+++ packages/web-app/src/containers/smartContractComposer/components/inputForm.tsx
@@ -168,14 +168,14 @@ const InputForm: React.FC<InputFormProps> = ({
 
   if (!selectedAction) {
     return (
-      <div className="desktop:p-6 min-h-full desktop:bg-white bg-ui-50">
+      <div className="desktop:p-6 min-h-full bg-ui-50 desktop:bg-white">
         Sorry, no public Write functions were found for this contract.
       </div>
     );
   }
 
   return (
-    <div className="desktop:p-6 min-h-full desktop:bg-white bg-ui-50">
+    <div className="desktop:p-6 min-h-full bg-ui-50 desktop:bg-white">
       <div className="desktop:flex items-baseline space-x-3">
         <ActionName>{selectedAction.name}</ActionName>
         <div className="hidden desktop:flex items-center space-x-1 text-primary-600">
@@ -191,10 +191,10 @@ const InputForm: React.FC<InputFormProps> = ({
         <IconSuccess />
       </div>
       {actionInputs.length > 0 ? (
-        <div className="p-3 mt-5 space-y-2 bg-white rounded-xl border desktop:bg-ui-50 border-ui-100 shadow-100">
+        <div className="p-3 mt-5 space-y-2 bg-white desktop:bg-ui-50 rounded-xl border border-ui-100 shadow-100">
           {actionInputs.map(input => (
             <div key={input.name}>
-              <div className="text-base font-bold capitalize text-ui-800">
+              <div className="text-base font-bold text-ui-800 capitalize">
                 {input.name}
                 <span className="ml-0.5 text-sm normal-case">
                   ({input.type})
@@ -346,15 +346,23 @@ export const ComponentForType: React.FC<ComponentForTypeProps> = ({
           }) => (
             <NumberInput
               name={name}
-              onBlur={onBlur}
+              onBlur={() => {
+                if (value) onChange(augmentUint256FieldValue(value));
+                onBlur();
+              }}
               onChange={e => {
-                onChange({
-                  ...e,
-                  target: {
-                    ...e.target,
-                    value: augmentUint256FieldValue(e.target.value),
-                  },
-                });
+                /* To augment uint256 with decimals when increment/decrement clicked */
+                const prevValue = value || '0';
+                const newValue = e.target.value;
+                const isControlsUsed =
+                  newValue &&
+                  Math.abs(Number(prevValue) - Number(newValue)) === 1;
+
+                if (isControlsUsed) {
+                  onChange(augmentUint256FieldValue(newValue));
+                } else {
+                  onChange(newValue);
+                }
               }}
               placeholder="0"
               includeDecimal
@@ -371,7 +379,7 @@ export const ComponentForType: React.FC<ComponentForTypeProps> = ({
         <>
           {input.components?.map(component => (
             <div key={component.name}>
-              <div className="mb-1.5 text-base font-bold capitalize text-ui-800">
+              <div className="mb-1.5 text-base font-bold text-ui-800 capitalize">
                 {input.name}
               </div>
               <ComponentForType
