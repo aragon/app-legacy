3dd6c713d7501d4602f15e48e70d6f0b6cd600f5
Fabrice Francois <fabricevladimir@gmail.com>
2023-06-30 09:38:04 -0400

    added todo comment

--- packages/web-app/src/utils/constants/chains.ts
+++ packages/web-app/src/utils/constants/chains.ts
@@ -126,6 +126,7 @@ export const CHAIN_METADATA: ChainList = {
     covalentApi: 'https://api.covalenthq.com/v1/eth-mainnet',
     alchemyApi: 'https://eth-mainnet.g.alchemy.com/v2',
     supportsEns: true,
+    //TODO: revert back when issue is fixed
     // ipfs: 'https://prod.ipfs.aragon.network',
     ipfs: 'https://test.ipfs.aragon.network',
   },
@@ -150,6 +151,7 @@ export const CHAIN_METADATA: ChainList = {
     covalentApi: 'https://api.covalenthq.com/v1/matic-mainnet',
     alchemyApi: 'https://polygon-mainnet.g.alchemy.com/v2',
     supportsEns: false,
+    //TODO: revert back when issue is fixed
     // ipfs: 'https://prod.ipfs.aragon.network',
     ipfs: 'https://test.ipfs.aragon.network',
   },
