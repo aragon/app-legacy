8d7f0ba27772f575300809e53e26b4104eed166c
Ruggero Cino <ruggero.cino@gmail.com>
2023-07-04 13:07:34 +0200

    Only use esm format, remove redundant replace config

--- packages/ui-components/tsdx.config.js
+++ packages/ui-components/tsdx.config.js
@@ -1,22 +1,11 @@
 const images = require('@rollup/plugin-image');
 const postcss = require('rollup-plugin-postcss');
-const replace = require('@rollup/plugin-replace');
-const { uglify } = require('rollup-plugin-uglify');
+const {uglify} = require('rollup-plugin-uglify');
 
 module.exports = {
-  rollup(config, opts) {
-    // suppress prevent assignment warning
-    config.plugins = config.plugins.map(plugin =>
-      plugin.name === 'replace'
-        ? replace({
-            'process.env.NODE_ENV': JSON.stringify(opts.env),
-            preventAssignment: true,
-          })
-        : plugin
-    );
-
-    // postcss config
-    config.plugins.push(
+  rollup(config) {
+    config.plugins = [
+      // postcss config
       postcss({
         config: {
           path: './postcss.config.js',
@@ -26,16 +15,16 @@ module.exports = {
         inject: {
           insertAt: 'top',
         },
-      })
-    );
+      }),
 
-    // plugin for bundling images
-    config.plugins = [
+      // plugin for bundling images
       images({include: ['**/*.png', '**/*.jpg', '**/*.svg']}),
+
+      // uglify code
+      uglify(),
+
       ...config.plugins,
     ];
-
-    config.plugins.push(uglify());
 
     return config;
   },
