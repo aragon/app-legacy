ed02b9336f99314c1b9c571fd4b9b177a49a48a7
Fabrice Francois <fabricevladimir@gmail.com>
2023-07-11 07:10:50 -0400

    addAddressCard update

--- packages/web-app/src/components/executionWidget/actions/addAddressCard.tsx
+++ packages/web-app/src/components/executionWidget/actions/addAddressCard.tsx
@@ -1,12 +1,14 @@
-import {ListItemAddress} from '@aragon/ui-components';
-import React from 'react';
+import {ListItemAddress} from '@aragon/ui-components/src/components/listItem/address';
+import React, {useCallback, useEffect, useState} from 'react';
 import {useTranslation} from 'react-i18next';
 import styled from 'styled-components';
 
 import {AccordionMethod} from 'components/accordionMethod';
 import AccordionSummary from 'containers/actionBuilder/addAddresses/accordionSummary';
 import {useNetwork} from 'context/network';
+import {useProviders} from 'context/providers';
 import {CHAIN_METADATA} from 'utils/constants';
+import {Web3Address} from 'utils/library';
 import {ActionAddAddress} from 'utils/types';
 
 export const AddAddressCard: React.FC<{
@@ -14,8 +16,44 @@ export const AddAddressCard: React.FC<{
 }> = ({action}) => {
   const {t} = useTranslation();
   const {network} = useNetwork();
+  const {infura: provider} = useProviders();
   const filteredMemberWallets = action.inputs.memberWallets.filter(
     wallet => wallet.address
+  );
+
+  const [displayedWallets, setDisplayedWallets] = useState<Web3Address[]>([]);
+
+  useEffect(() => {
+    async function filterMembers() {
+      let wallets: ActionAddAddress['inputs']['memberWallets'] | Web3Address[] =
+        action.inputs.memberWallets.filter(wallet => wallet.address);
+
+      try {
+        wallets = await Promise.all(
+          wallets.map(async wallet => {
+            return await Web3Address.create(provider, {
+              address: wallet.address as string,
+              ensName: wallet.ensName as string,
+            });
+          })
+        );
+      } catch (error) {
+        console.error('Error creating Web3Addresses', error);
+      }
+
+      setDisplayedWallets(wallets as Array<Web3Address>);
+    }
+
+    if (action.inputs.memberWallets) filterMembers();
+  }, [action.inputs.memberWallets, provider]);
+
+  const handleAddressClick = useCallback(
+    (addressOrEns: string | null) =>
+      window.open(
+        `${CHAIN_METADATA[network].explorer}address/${addressOrEns}`,
+        '_blank'
+      ),
+    [network]
   );
 
   return (
@@ -27,17 +65,12 @@ export const AddAddressCard: React.FC<{
       methodDescription={t('labels.addWalletsDescription')}
     >
       <Container>
-        {filteredMemberWallets.map(({address}) => (
+        {displayedWallets.map(({address, avatar, ensName}) => (
           <ListItemAddress
-            label={address}
-            src={address}
+            label={ensName || address}
+            src={avatar || address}
             key={address}
-            onClick={() =>
-              window.open(
-                `${CHAIN_METADATA[network].explorer}address/${address}`,
-                '_blank'
-              )
-            }
+            onClick={() => handleAddressClick(ensName || address)}
           />
         ))}
       </Container>
