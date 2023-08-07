77f8bdf75acfffdf36fd6404111aace2c27bf3e0
selankon <selankon@selankon.xyz>
2023-08-07 13:05:17 +0200

    Create vocdoni census3

--- src/context/createDao.tsx
+++ src/context/createDao.tsx
@@ -45,6 +45,8 @@ import {CreateDaoFormData} from 'utils/types';
 import {useGlobalModalContext} from './globalModals';
 import {useNetwork} from './network';
 
+import {useClient as useVocdoniClient} from 'hooks/useVocdoniSdk';
+
 const DEFAULT_TOKEN_DECIMALS = 18;
 
 type CreateDaoContextType = {
@@ -364,6 +366,13 @@ const CreateDaoProvider: React.FC<{children: ReactNode}> = ({children}) => {
     error: gasEstimationError,
   } = usePollGasFee(estimateCreationFees, shouldPoll);
 
+  const {census3Client} = useVocdoniClient();
+
+  // It creates the vocdoni census3 for a specific DAO
+  const createDAOCensus3 = useCallback(async () => {
+    await census3Client.createToken(daoAddress, 'ERC20');
+  }, [census3Client, daoAddress]);
+
   // run dao creation transaction
   const createDao = async () => {
     setCreationProcessState(TransactionState.LOADING);
@@ -379,7 +388,7 @@ const CreateDaoProvider: React.FC<{children: ReactNode}> = ({children}) => {
       throw new Error('deposit function is not initialized correctly');
     }
 
-    const {daoName, daoSummary, daoLogo, links} = getValues();
+    const {daoName, daoSummary, daoLogo, links, votingType} = getValues();
     const metadata: DaoMetadata = {
       name: daoName,
       description: daoSummary,
@@ -436,7 +445,15 @@ const CreateDaoProvider: React.FC<{children: ReactNode}> = ({children}) => {
                     },
                   },
                 }),
-              ]);
+              ]).then(async () => {
+                if (
+                  votingType === 'offChain' &&
+                  !(await census3Client.getToken(step.address.toLowerCase()))
+                ) {
+                  await createDAOCensus3();
+                }
+              });
+              // After everything is
             } catch (error) {
               console.warn(
                 'Error favoriting and adding newly created DAO to cache',
