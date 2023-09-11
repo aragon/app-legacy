b9f8410a382fc95a852d21f435da5f329b856870
Ruggero Cino <ruggero.cino@gmail.com>
2023-09-11 09:31:45 +0200

    Restore favourite dao functionalities

--- src/services/cache.ts
+++ src/services/cache.ts
@@ -41,6 +41,28 @@ export async function getFavoritedDaosFromCache(options: {
 }
 
 /**
+ * Fetch a favorited DAO from the cache if available
+ * @param daoAddress the address of the favorited DAO to fetch
+ * @param chain the chain of the favorited DAO to fetch
+ * @returns a favorited DAO with the given address and chain or null
+ * if not found
+ */
+export async function getFavoritedDaoFromCache(
+  daoAddress: string | undefined,
+  chain: SupportedChainID
+) {
+  if (!daoAddress)
+    return Promise.reject(new Error('daoAddressOrEns must be defined'));
+
+  if (!chain) return Promise.reject(new Error('chain must be defined'));
+
+  const daos = await getFavoritedDaosFromCache({skip: 0});
+  return (
+    daos.find(dao => dao.address === daoAddress && dao.chain === chain) ?? null
+  );
+}
+
+/**
  * Favorite a DAO by adding it to the favorite DAOs cache
  * @param dao DAO being favorited
  * @returns an error if the dao to favorite is not provided
@@ -70,6 +92,27 @@ export async function removeFavoriteDaoFromCache(dao: NavigationDao) {
   );
 
   localStorage.setItem(FAVORITE_DAOS_KEY, JSON.stringify(newCache));
+}
+
+/**
+ * Update a DAO in the cache
+ * @param dao updated DAO; note dao.address & dao.chain should never be changed
+ * @returns an error if no DAO is provided
+ */
+export async function updateFavoritedDaoInCache(dao: NavigationDao) {
+  if (!dao) return Promise.reject(new Error('dao must be defined'));
+
+  const cache = await getFavoritedDaosFromCache({skip: 0});
+  const daoFound = cache.findIndex(
+    d => d.address === dao.address && d.chain === dao.chain
+  );
+
+  if (daoFound !== -1) {
+    const newCache = [...cache];
+    newCache[daoFound] = {...dao};
+
+    localStorage.setItem(FAVORITE_DAOS_KEY, JSON.stringify(newCache));
+  }
 }
 
 /**
