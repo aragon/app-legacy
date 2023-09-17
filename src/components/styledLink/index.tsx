7c4d40f1501eefdeee78b5360982ee7d46ce5513
sepehr sanaee <sepehr2mail@gmail.com>
2023-09-17 21:33:03 +0330

    Fix StyledLink color css

--- src/components/styledLink/index.tsx
+++ src/components/styledLink/index.tsx
@@ -4,11 +4,11 @@ import styled from 'styled-components';
 // Temporary local component until an inverse variant is introduced into the
 // ODS link
 export const StyledLink = styled(Link as React.ComponentType<LinkProps>)`
-  & > div > span {
+  & > span {
     color: #ffffff; // ui-0
   }
 
-  & > div > div > svg {
+  & > div > svg {
     color: #ffffff;
   }
 
