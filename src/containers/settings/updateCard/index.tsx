0c1d411f90e8f4e4d8d073f1925fbcd8d36c670a
sepehr sanaee <sepehr2mail@gmail.com>
2023-09-17 21:22:02 +0330

    Added fetchProtocolVersions

--- src/containers/settings/updateCard/index.tsx
+++ src/containers/settings/updateCard/index.tsx
@@ -16,7 +16,7 @@ export const SettingsUpdateCard: React.FC = () => {
         <div className="flex gap-x-6 items-start">
           <div className="flex-1 space-y-1">
             <Head>
-              <IconUpdate />
+              {/* <IconUpdate /> */}
               <Title>{t('update.alert.title')}</Title>
             </Head>
             <ContentWrapper className="space-y-0">
