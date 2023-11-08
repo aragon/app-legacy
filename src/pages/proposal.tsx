omahs/app-2

Mock ui to show committe approval voting

src/pages/proposal.tsx
@@ -86,6 +86,7 @@ import {format} from 'date-fns';
 import {getFormattedUtcOffset, KNOWN_FORMATS} from '../utils/date';
 import {formatUnits, IChoice} from '@vocdoni/sdk';
 import Big from 'big.js';
+import {OffchainVotingTerminal} from '../containers/votingTerminal/offchainVotingTerminal';
 
 // TODO: @Sepehr Please assign proper tags on action decoding
 // const PROPOSAL_TAGS = ['Finance', 'Withdraw'];
@@ -741,6 +742,26 @@ const ProposalPage: React.FC<IProposalPage> = ({proposalId}: IProposalPage) => {
     return <Loading />;
   }
 
+  // Store the terminal to pass it to OffchainVotingTerminal if needed
+  const VTerminal = () => (
+    <VotingTerminal
+      status={proposal.status}
+      statusLabel={voteStatus}
+      selectedTab={terminalTab}
+      alertMessage={alertMessage}
+      onTabSelected={setTerminalTab}
+      onVoteClicked={onClick}
+      onCancelClicked={() => setVotingInProcess(false)}
+      voteButtonLabel={buttonLabel}
+      voteNowDisabled={voteNowDisabled}
+      votingInProcess={votingInProcess}
+      onVoteSubmitClicked={vote =>
+        handleSubmitVote(vote, (proposal as TokenVotingProposal).token?.address)
+      }
+      {...mappedProps}
+    />
+  );
+
   return (
     <Container>
       <HeaderContainer>
@@ -808,25 +829,16 @@ const ProposalPage: React.FC<IProposalPage> = ({proposalId}: IProposalPage) => {
             </>
           )}
 
-          <VotingTerminal
-            status={proposal.status}
-            statusLabel={voteStatus}
-            selectedTab={terminalTab}
-            alertMessage={alertMessage}
-            onTabSelected={setTerminalTab}
-            onVoteClicked={onClick}
-            onCancelClicked={() => setVotingInProcess(false)}
-            voteButtonLabel={buttonLabel}
-            voteNowDisabled={voteNowDisabled}
-            votingInProcess={votingInProcess}
-            onVoteSubmitClicked={vote =>
-              handleSubmitVote(
-                vote,
-                (proposal as TokenVotingProposal).token?.address
-              )
-            }
-            {...mappedProps}
-          />
+          {/*todo(kon): set the condition properly (if offchain, TokenVotingProposal*/}
+          {offChain ? (
+            <OffchainVotingTerminal
+              votingStatusLabel={voteStatus}
+              votingTerminal={<VTerminal />}
+              proposal={proposal as TokenVotingProposal}
+            />
+          ) : (
+            <VTerminal />
+          )}
 
           <ExecutionWidget
             pluginType={pluginType}

aragon/app-legacy

src/pages/proposal.tsx
@@ -83,6 +83,7 @@ import {format} from 'date-fns';
 import {getFormattedUtcOffset, KNOWN_FORMATS} from '../utils/date';
 import {formatUnits, IChoice} from '@vocdoni/sdk';
 import Big from 'big.js';
+import {OffchainVotingTerminal} from '../containers/votingTerminal/offchainVotingTerminal';
 
 const PENDING_PROPOSAL_STATUS_INTERVAL = 1000 * 10;
 const PROPOSAL_STATUS_INTERVAL = 1000 * 60;
@@ -622,6 +623,36 @@ export const Proposal: React.FC = () => {
     return null;
   }
 
+  // Store the terminal to pass it to OffchainVotingTerminal if needed
+  const VTerminal = () => (
+    <VotingTerminal
+      status={proposalStatus}
+      pluginType={pluginType}
+      daoToken={daoToken}
+      blockNumber={proposal.creationBlockNumber}
+      statusLabel={voteStatus}
+      selectedTab={terminalTab}
+      alertMessage={alertMessage}
+      onTabSelected={setTerminalTab}
+      onVoteClicked={handleVoteClick}
+      onApprovalClicked={handleApprovalClick}
+      onCancelClicked={() => setVotingInProcess(false)}
+      voteButtonLabel={voteButtonLabel}
+      voteNowDisabled={votingDisabled}
+      votingInProcess={votingInProcess}
+      executableWithNextApproval={executableWithNextApproval}
+      onVoteSubmitClicked={vote =>
+        handlePrepareVote({
+          vote,
+          replacement: voted || voteOrApprovalSubmitted,
+          voteTokenAddress: (proposal as TokenVotingProposal).token
+            ?.address,
+        })
+      }
+      {...mappedProps}
+    />
+  );
+
   return (
     <Container>
       <HeaderContainer>
@@ -694,33 +725,15 @@ export const Proposal: React.FC = () => {
               />
             )}
 
-          {votingSettings && (
-            <VotingTerminal
-              status={proposalStatus}
-              pluginType={pluginType}
-              daoToken={daoToken}
-              blockNumber={proposal.creationBlockNumber}
-              statusLabel={voteStatus}
-              selectedTab={terminalTab}
-              alertMessage={alertMessage}
-              onTabSelected={setTerminalTab}
-              onVoteClicked={handleVoteClick}
-              onApprovalClicked={handleApprovalClick}
-              onCancelClicked={() => setVotingInProcess(false)}
-              voteButtonLabel={voteButtonLabel}
-              voteNowDisabled={votingDisabled}
-              votingInProcess={votingInProcess}
-              executableWithNextApproval={executableWithNextApproval}
-              onVoteSubmitClicked={vote =>
-                handlePrepareVote({
-                  vote,
-                  replacement: voted || voteOrApprovalSubmitted,
-                  voteTokenAddress: (proposal as TokenVotingProposal).token
-                    ?.address,
-                })
-              }
-              {...mappedProps}
+          {/*todo(kon): set the condition properly (if offchain, TokenVotingProposal*/}
+          {votingSettings && offChain ? (
+            <OffchainVotingTerminal
+              votingStatusLabel={voteStatus}
+              votingTerminal={<VTerminal />}
+              proposal={proposal as TokenVotingProposal}
             />
+          ) : (
+            <VTerminal />
           )}
 
           <ExecutionWidget