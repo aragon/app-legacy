b05baa8c26008e76e292adee214bb6d58c8ce295
Fabrice Francois <fabricevladimir@gmail.com>
2023-11-27 09:53:32 -0500

    Proposal count in proposal snapshot

--- src/containers/proposalSnapshot/index.tsx
+++ src/containers/proposalSnapshot/index.tsx
@@ -28,6 +28,7 @@ import {htmlIn} from 'utils/htmlIn';
 import {Governance, NewProposal} from 'utils/paths';
 import {ProposalTypes} from 'utils/types';
 import {useIsUpdateProposal} from 'hooks/useIsUpdateProposal';
+import {useTotalProposalCount} from 'services/aragon-subgraph/queries/use-total-proposal-count';
 
 type Props = {
   daoAddressOrEns: string;
@@ -68,14 +69,19 @@ const ProposalSnapshot: React.FC<Props> = ({
   const {address} = useWallet();
   const {network} = useNetwork();
 
-  const {
-    data,
-    isFetched: proposalsFetched,
-    isLoading: proposalsAreLoading,
-  } = useProposals({
+  const {data, isLoading: proposalsAreLoading} = useProposals({
     daoAddressOrEns,
     pluginType,
     pluginAddress,
+  });
+
+  const {
+    data: proposalCount,
+    isLoading: proposalCountIsLoading,
+    isFetched: proposalCountIsFetched,
+  } = useTotalProposalCount({
+    pluginAddress,
+    pluginType,
   });
 
   const {data: members} = useDaoMembers(pluginAddress, pluginType, {
@@ -97,11 +103,11 @@ const ProposalSnapshot: React.FC<Props> = ({
       );
     });
 
-  if (proposalsAreLoading) {
+  if (proposalsAreLoading || proposalCountIsLoading) {
     return <Loading />;
   }
 
-  if ((proposalsFetched && mappedProposals?.length === 0) || !mappedProposals) {
+  if (proposalCountIsFetched && proposalCount === 0) {
     return (
       <StateEmpty
         type="Human"
@@ -133,7 +139,7 @@ const ProposalSnapshot: React.FC<Props> = ({
     <Container>
       <ListItemHeader
         icon={<IconGovernance />}
-        value={mappedProposals.length.toString()}
+        value={proposalCount?.toString() ?? '0'}
         label={t('dashboard.proposalsTitle')}
         buttonText={t('newProposal.title')}
         orientation="horizontal"
@@ -148,7 +154,7 @@ const ProposalSnapshot: React.FC<Props> = ({
         }
       />
 
-      {mappedProposals.map(({id, ...p}) => (
+      {mappedProposals?.map(({id, ...p}) => (
         <ProposalItem {...p} proposalId={id} key={id} type="list" />
       ))}
 
