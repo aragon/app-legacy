8ebb7c8e4ff5d7c49f15bee46aba4eb74b473c71
selankon <selankon@selankon.xyz>
2023-12-01 12:34:14 +0100

    Implement gasless action filter

---
+++ src/components/executionWidget/actions/modifyGaslessSettingsCard.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import {useTranslation} from 'react-i18next';
+
+import {AccordionMethod} from 'components/accordionMethod';
+import {ActionCardDlContainer, Dd, Dl, Dt} from 'components/descriptionList';
+import {
+  ActionUpdateGaslessSettings,
+  ActionUpdateMultisigPluginSettings,
+} from 'utils/types';
+import {Link} from '../../../@aragon/ods-old';
+import CommitteeAddressesModal from '../../../containers/committeeAddressesModal';
+import {getDHMFromSeconds} from '../../../utils/date';
+
+export const ModifyGaslessSettingsCard: React.FC<{
+  action: ActionUpdateGaslessSettings;
+}> = ({action: {inputs}}) => {
+  const {t} = useTranslation();
+  const {days, hours, minutes} = getDHMFromSeconds(inputs.minDuration);
+
+  return (
+    <AccordionMethod
+      type="execution-widget"
+      methodName={t('labels.updateGaslessAction')}
+      smartContractName={t('labels.aragonOSx')}
+      methodDescription={t('labels.updateGaslessActionDescription')}
+      verified
+    >
+      <ActionCardDlContainer>
+        <Dl>
+          <Dt>{t('labels.members')}</Dt>
+          <Dd>
+            <Link
+              label={t('createDAO.review.distributionLink', {
+                count: inputs.executionMultisigMembers?.length || 0,
+              })}
+              onClick={() => open('committeeMembers')}
+            />
+          </Dd>
+        </Dl>
+        <Dl>
+          <Dt>{t('labels.minimumApproval')}</Dt>
+          <Dd>
+            {inputs.minTallyApprovals}&nbsp;
+            {t('labels.review.multisigMinimumApprovals', {
+              count: inputs.executionMultisigMembers?.length || 0,
+            })}
+          </Dd>
+        </Dl>
+        <Dl>
+          <Dt>{t('createDao.executionMultisig.executionTitle')}</Dt>
+          <Dd>
+            <div className="flex space-x-1.5">
+              <div>{t('createDAO.review.days', {days: days})}</div>
+              {hours && hours! > 0 && (
+                <div>
+                  {t('createDAO.review.hours', {
+                    hours: hours,
+                  })}
+                </div>
+              )}
+              {minutes && minutes! > 0 && (
+                <div>
+                  {t('createDAO.review.minutes', {
+                    minutes: minutes,
+                  })}
+                </div>
+              )}
+            </div>
+          </Dd>
+        </Dl>
+        <CommitteeAddressesModal />
+      </ActionCardDlContainer>
+    </AccordionMethod>
+  );
+};
