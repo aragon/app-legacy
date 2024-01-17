2692c754d6a7ea9fa3b3d6badd6a6f6f0728fc26
Ruggero Cino <ruggero.cino@gmail.com>
2024-01-17 14:46:29 +0100

    Update providers handling to use new gateway RPC

---
+++ src/utils/aragonGateway.ts
@@ -0,0 +1,60 @@
+import {
+  LIVE_CONTRACTS,
+  SupportedVersion,
+  SupportedNetwork as SdkSupportedNetworks,
+} from '@aragon/sdk-client-common';
+import {JsonRpcProvider, Networkish} from '@ethersproject/providers';
+import {
+  CHAIN_METADATA,
+  NETWORKS_WITH_CUSTOM_REGISTRY,
+  SupportedChainID,
+  SupportedNetworks,
+  getSupportedNetworkByChainId,
+  infuraApiKey,
+} from './constants';
+import {translateToNetworkishName} from './library';
+
+class AragonGateway {
+  private rpcVersion = '1.0';
+
+  getRpcClient = (
+    chainIdOrNetwork: SupportedChainID | SupportedNetworks
+  ): JsonRpcProvider | null => {
+    const network =
+      typeof chainIdOrNetwork === 'number'
+        ? getSupportedNetworkByChainId(chainIdOrNetwork)
+        : chainIdOrNetwork;
+
+    if (network == null || network === 'unsupported') {
+      return null;
+    }
+
+    const sdkNetwork = translateToNetworkishName(
+      network
+    ) as SdkSupportedNetworks;
+
+    const options: Networkish = {
+      chainId: CHAIN_METADATA[network].id,
+      name: sdkNetwork,
+    };
+
+    if (NETWORKS_WITH_CUSTOM_REGISTRY.includes(network)) {
+      options.ensAddress =
+        LIVE_CONTRACTS[SupportedVersion.LATEST][sdkNetwork]?.ensRegistryAddress;
+    }
+
+    const rpcUrl = this.buildRpcUrl(network);
+
+    return new JsonRpcProvider(rpcUrl, options);
+  };
+
+  buildRpcUrl = (network: SupportedNetworks) => {
+    const {gatewayNetwork} = CHAIN_METADATA[network];
+    const baseUrl = import.meta.env.VITE_GATEWAY_URL;
+    const rpcUrl = `${baseUrl}/v1.0/rpc/${gatewayNetwork}/${infuraApiKey}`;
+
+    return rpcUrl;
+  };
+}
+
+export const aragonGateway = new AragonGateway();
