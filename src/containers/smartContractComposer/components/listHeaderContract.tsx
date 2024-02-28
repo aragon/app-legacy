a858babdaf084d06ee667dff2e13c036f61ec2cc
sepehr sanaee <sepehr2mail@gmail.com>
2024-02-28 15:23:30 +0330

    ListHeaderContract Dropdown updated

--- src/containers/smartContractComposer/components/listHeaderContract.tsx
+++ src/containers/smartContractComposer/components/listHeaderContract.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import {
-  Dropdown,
   Link,
   ListItemAction,
   ListItemActionProps,
   shortenAddress,
 } from '@aragon/ods-old';
-import {Icon, IconType} from '@aragon/ods';
+import {Icon, IconType, Dropdown} from '@aragon/ods';
 import {useFormContext} from 'react-hook-form';
 import {useTranslation} from 'react-i18next';
 
@@ -37,60 +36,87 @@ export const ListHeaderContract: React.FC<Props> = ({
   const contracts = getValues('contracts');
 
   const listItems = [
-    {
-      component: (
-        <Link
-          external
-          type="neutral"
-          iconRight={
-            <Icon icon={IconType.LINK_EXTERNAL} size="md" className="ml-8" />
-          }
-          href={chainExplorerAddressLink(network, sc.address) + '#code'}
-          label={t('scc.detailContract.dropdownExplorerLinkLabel', {
-            address: sc.address,
-          })}
-          className="my-2 w-full justify-between px-4"
-        />
-      ),
-      callback: () => {},
-    },
-    {
-      component: (
-        <Link
-          external
-          type="neutral"
-          iconRight={<Icon icon={IconType.COPY} size="md" className="ml-8" />}
-          label={t('scc.detailContract.dropdownCopyLabel')}
-          className="my-2 w-full justify-between px-4"
-        />
-      ),
-      callback: () => {
+    <Dropdown.Item key={0}>
+      <Link
+        external
+        type="neutral"
+        iconRight={
+          <Icon icon={IconType.LINK_EXTERNAL} size="md" className="ml-8" />
+        }
+        href={chainExplorerAddressLink(network, sc.address) + '#code'}
+        label={t('scc.detailContract.dropdownExplorerLinkLabel', {
+          address: sc.address,
+        })}
+        className="my-2 w-full justify-between px-4"
+      />
+    </Dropdown.Item>,
+    <Dropdown.Item
+      key={1}
+      onClick={() => {
         handleClipboardActions(sc.address, () => {}, alert);
-      },
-    },
-    {
-      component: (
-        <Link
-          external
-          type="neutral"
-          iconRight={<Icon icon={IconType.CLOSE} size="md" className="ml-8" />}
-          label={t('scc.detailContract.dropdownRemoveLabel')}
-          className="my-2 w-full justify-between px-4"
-        />
-      ),
-      callback: () => {
+      }}
+    >
+      <Link
+        external
+        type="neutral"
+        iconRight={<Icon icon={IconType.COPY} size="md" className="ml-8" />}
+        label={t('scc.detailContract.dropdownCopyLabel')}
+        className="my-2 w-full justify-between px-4"
+      />
+    </Dropdown.Item>,
+    <Dropdown.Item
+      key={2}
+      onClick={() => {
         if (sc.implementationData) {
           onRemoveContract(sc.proxyAddress as string);
         } else {
           onRemoveContract(sc.address);
         }
-      },
-    },
+      }}
+    >
+      <Link
+        external
+        type="neutral"
+        iconRight={<Icon icon={IconType.CLOSE} size="md" className="ml-8" />}
+        label={t('scc.detailContract.dropdownRemoveLabel')}
+        className="my-2 w-full justify-between px-4"
+      />
+    </Dropdown.Item>,
   ];
 
   if (sc.proxyAddress || sc.implementationData) {
-    listItems.unshift({
-      component: (
+    listItems.unshift(
+      <Dropdown.Item
+        key={2}
+        onClick={() => {
+          if (sc.implementationData) {
+            setValue('selectedSC', sc.implementationData as SmartContract);
+            setValue(
+              'selectedAction',
+              (sc.implementationData as SmartContract).actions.filter(
+                a =>
+                  a.type === 'function' &&
+                  (a.stateMutability === 'payable' ||
+                    a.stateMutability === 'nonpayable')
+              )?.[0]
+            );
+          } else {
+            const contract = contracts.filter(
+              c => c.address === sc.proxyAddress
+            )[0];
+            setValue('selectedSC', contract);
+            setValue(
+              'selectedAction',
+              contract.actions.filter(
+                a =>
+                  a.type === 'function' &&
+                  (a.stateMutability === 'payable' ||
+                    a.stateMutability === 'nonpayable')
+              )?.[0]
+            );
+          }
+        }}
+      >
         <Link
           external
           type="neutral"
@@ -102,49 +128,21 @@ export const ListHeaderContract: React.FC<Props> = ({
           iconRight={<Icon icon={IconType.BLOCKCHAIN_SMARTCONTRACT} />}
           className="my-2 w-full justify-between px-4"
         />
-      ),
-      callback: () => {
-        if (sc.implementationData) {
-          setValue('selectedSC', sc.implementationData as SmartContract);
-          setValue(
-            'selectedAction',
-            (sc.implementationData as SmartContract).actions.filter(
-              a =>
-                a.type === 'function' &&
-                (a.stateMutability === 'payable' ||
-                  a.stateMutability === 'nonpayable')
-            )?.[0]
-          );
-        } else {
-          const contract = contracts.filter(
-            c => c.address === sc.proxyAddress
-          )[0];
-          setValue('selectedSC', contract);
-          setValue(
-            'selectedAction',
-            contract.actions.filter(
-              a =>
-                a.type === 'function' &&
-                (a.stateMutability === 'payable' ||
-                  a.stateMutability === 'nonpayable')
-            )?.[0]
-          );
-        }
-      },
-    });
+      </Dropdown.Item>
+    );
   }
 
   const iconRight = (
-    <Dropdown
+    <Dropdown.Container
       align="start"
-      trigger={
+      customTrigger={
         <button>
           <Icon icon={IconType.DOTS_VERTICAL} />
         </button>
       }
-      sideOffset={8}
-      listItems={listItems}
-    />
+    >
+      {listItems}
+    </Dropdown.Container>
   );
 
   const liaProps = {
