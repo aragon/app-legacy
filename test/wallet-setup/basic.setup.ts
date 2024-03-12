5a7fba742984f6cad60e108431562fac15594a0c
Barukimang <barukimang@gmail.com>
2024-03-12 17:18:36 +0300

    added wallet setup file for caching

---
+++ test/wallet-setup/basic.setup.ts
@@ -0,0 +1,16 @@
+import {MetaMask, defineWalletSetup} from '@synthetixio/synpress';
+// import 'dotenv/config';
+
+// const SEED_PHRASE = import.meta.env.SEED_PHRASE;
+// const PASSWORD = import.meta.env.PASSWORD;
+
+const SEED_PHRASE =
+  'clay risk pepper raccoon card erode desert index short glide junk fox';
+const PASSWORD = 'testitesti';
+console.log({SEED_PHRASE, PASSWORD});
+
+export default defineWalletSetup(PASSWORD, async (context, walletPage) => {
+  const metamask = new MetaMask(context, walletPage, PASSWORD);
+
+  await metamask.importWallet(SEED_PHRASE);
+});
