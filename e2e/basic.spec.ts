71ca1735bbb126efdc146c2afd80b17efa4afbe4
Barukimang <barukimang@gmail.com>
2024-03-12 19:21:12 +0300

    added wallet setup fixture

--- e2e/basic.spec.ts
+++ e2e/basic.spec.ts
@@ -17,42 +17,65 @@
 //   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 // });
 
-import {
-  MetaMask,
-  testWithSynpress,
-  unlockForFixture,
-} from '@synthetixio/synpress';
-import BasicSetup from '../test/wallet-setup/basic.setup.ts';
+// import {
+//   MetaMask,
+//   testWithSynpress,
+//   unlockForFixture,
+// } from '@synthetixio/synpress';
+// import BasicSetup from '../test/wallet-setup/basic.setup.ts';
 
-const test = testWithSynpress(BasicSetup, unlockForFixture);
+// const test = testWithSynpress(BasicSetup, unlockForFixture);
+
+// const {expect} = test;
+
+// test('should connect wallet to the MetaMask Test Dapp', async ({
+//   context,
+//   page,
+//   extensionId,
+// }) => {
+//   const metamask = new MetaMask(
+//     context,
+//     page,
+//     BasicSetup.walletPassword,
+//     extensionId
+//   );
+
+//   await page.goto('http://localhost:3000/');
+//   await page.getByRole('button', {name: 'Accept all'}).click();
+//   await page.getByRole('button', {name: 'Connect wallet'}).click();
+
+//   await page.goto('/');
+//   await page.locator('#connectButton').click();
+//   await metamask.connectToDapp();
+//   await expect(page.locator('#accounts')).toHaveText(
+//     '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'
+//   );
+
+//   await page.locator('#getAccounts').click();
+//   await expect(page.locator('#getAccountsResult')).toHaveText(
+//     '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'
+//   );
+// });
+
+import {testWithMetaMask as test} from './testWithMetaMask';
 
 const {expect} = test;
 
-test('should connect wallet to the MetaMask Test Dapp', async ({
+// The `MetaMask` instance is now available in the test context.
+test('should connect multiple wallets to dapp', async ({
   context,
   page,
   extensionId,
+  metamask,
 }) => {
-  const metamask = new MetaMask(
-    context,
-    page,
-    BasicSetup.walletPassword,
-    extensionId
-  );
-
   await page.goto('http://localhost:3000/');
   await page.getByRole('button', {name: 'Accept all'}).click();
   await page.getByRole('button', {name: 'Connect wallet'}).click();
-
   await page.goto('/');
-  await page.locator('#connectButton').click();
-  await metamask.connectToDapp();
-  await expect(page.locator('#accounts')).toHaveText(
-    '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'
-  );
 
-  await page.locator('#getAccounts').click();
-  await expect(page.locator('#getAccountsResult')).toHaveText(
-    '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'
-  );
+  await page.locator('#connectButton').click();
+
+  await metamask.connectToDapp(['0xdeadbeef1', '0xdeadbeef2']);
+
+  await expect(page.locator('#accounts')).toHaveText('0xdeadbeef1,0xdeadbeef2');
 });
