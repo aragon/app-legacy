e0f4b983263441a82b7c0de625652e7ef5f3d0ce
sepehr sanaee <sepehr2mail@gmail.com>
2024-04-01 18:45:38 +0330

    update corresponding comment

--- src/containers/smartContractComposer/components/contractAddressValidation.tsx
+++ src/containers/smartContractComposer/components/contractAddressValidation.tsx
@@ -271,7 +271,7 @@ const ContractAddressValidation: React.FC<Props> = props => {
           });
         }
 
-        //prioritize sourcify over etherscan if sourcify data is available
+        //prioritize etherscan over sourcify to support proxy contracts for sourcify verified contracts
         if (
           etherscanData.result[0].ABI !== 'Contract source code not verified'
         ) {
