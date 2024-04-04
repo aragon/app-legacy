c5beb9b042964e9dba8d1d5164155dd1e6554cff
sepehr sanaee <sepehr2mail@gmail.com>
2024-04-04 13:09:32 +0330

    disable syncConnectedChain

--- src/index.tsx
+++ src/index.tsx
@@ -68,13 +68,13 @@ const transports = chains.reduce(
 export const wagmiConfig = createConfig({
   chains,
   transports: transports,
+  syncConnectedChain: false,
   connectors: [
     walletConnect({
       projectId: walletConnectProjectID,
       metadata: AppMetadata,
       showQrModal: false,
     }),
-    injected({shimDisconnect: true}),
     coinbaseWallet({
       appName: AppMetadata.name,
       appLogoUrl: AppMetadata.icons[0],
@@ -94,7 +94,6 @@ createWeb3Modal({
     'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
     '1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369',
     '18388be9ac2d02726dbac9777c96efaac06d744b2f6d580fccdd4127a6d01fd1',
-    '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
   ],
 });
 
