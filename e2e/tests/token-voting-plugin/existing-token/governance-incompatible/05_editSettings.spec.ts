eb0e4ba8ad66b056b7f50c57e8f6dbd5ef4e8372
Barukimang <barukimang@gmail.com>
2024-04-10 11:30:43 +0300

    succesful create dao governance incompatible token test

--- e2e/tests/token-voting-plugin/existing-token/governance-incompatible/05_editSettings.spec.ts
+++ e2e/tests/token-voting-plugin/existing-token/governance-incompatible/05_editSettings.spec.ts
@@ -1,4 +1,4 @@
-import {testWithMetaMask as test} from '../../../testWithMetaMask';
+import {testWithMetaMask as test} from '../../../../testWithMetaMask';
 
 // // Test is publishing, approving, and executing a proposal to edit a DAO settings
 test('Edit DAO settings Proposal', async ({
