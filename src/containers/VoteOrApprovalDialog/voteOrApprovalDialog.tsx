e58c17dcc27f3045fcdb0166d34ae1aca3880301
sepehr sanaee <sepehr2mail@gmail.com>
2024-04-22 10:26:25 +0330

    Renamed voteOrApprovalDialog File

---
+++ src/containers/VoteOrApprovalDialog/voteOrApprovalDialog.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import {ModalProps} from '@aragon/ods-old';
+import {TransactionDialog} from 'containers/transactionDialog';
+import {useVoteOrApprovalTransaction} from 'services/transactions/queries/useVoteOrApprovalTransaction';
+import {voteOrApprovalUtils} from './utils/index';
+import {IBuildVoteOrApprovalTransactionParams} from 'services/transactions/transactionsService.api';
+import {useSendVoteOrApprovalTransaction} from './hooks';
+import {useTranslation} from 'react-i18next';
+import {PluginTypes, usePluginClient} from 'hooks/usePluginClient';
+import {VoteValues} from '@aragon/sdk-client';
+import {BigNumber} from 'ethers';
+
+/**
+ * Represents the props for the VoteOrApprovalDialog component.
+ */
+export interface IVoteOrApprovalDialogProps extends ModalProps {
+  pluginType: PluginTypes;
+  tryExecution: boolean;
+  vote?: VoteValues;
+  votingPower?: BigNumber;
+  replacingVote?: boolean;
+  voteTokenAddress?: string;
+  setVoteOrApprovalSubmitted: (value: boolean) => void;
+}
+
+const VoteOrApprovalProcess = 'VOTE_OR_APPROVAL';
+
+export const VoteOrApprovalDialog: React.FC<
+  IVoteOrApprovalDialogProps
+> = props => {
+  const {
+    isOpen,
+    onClose,
+    pluginType,
+    tryExecution,
+    replacingVote,
+    vote,
+    votingPower,
+    setVoteOrApprovalSubmitted,
+    ...otherProps
+  } = props;
+
+  const DialogTexts =
+    pluginType === 'multisig.plugin.dao.eth' ? 'vote' : 'approve';
+
+  const {t} = useTranslation();
+  const pluginClient = usePluginClient(pluginType as PluginTypes);
+
+  const VoteOrApprovalParams = voteOrApprovalUtils.buildVoteOrApprovalParams(
+    pluginType,
+    tryExecution,
+    vote
+  );
+
+  const {data: transaction} = useVoteOrApprovalTransaction(
+    {
+      ...VoteOrApprovalParams,
+      pluginClient,
+    } as IBuildVoteOrApprovalTransactionParams,
+    {enabled: VoteOrApprovalParams != null && pluginClient != null}
+  );
+
+  const sendTransactionResults = useSendVoteOrApprovalTransaction({
+    process: VoteOrApprovalProcess,
+    pluginType,
+    transaction,
+    votingPower,
+    replacingVote,
+    vote,
+    setVoteOrApprovalSubmitted,
+  });
+
+  return (
+    <TransactionDialog
+      title={t(`voteOrApprovalDialog.${DialogTexts}.title`)}
+      isOpen={isOpen}
+      sendTransactionResult={sendTransactionResults}
+      displayTransactionStatus={transaction != null}
+      sendTransactionLabel={`button.${DialogTexts}.approve`}
+      successButton={{
+        label: `button.${DialogTexts}.success`,
+        onClick: () => {
+          onClose?.();
+        },
+      }}
+      onClose={onClose}
+      {...otherProps}
+    />
+  );
+};
