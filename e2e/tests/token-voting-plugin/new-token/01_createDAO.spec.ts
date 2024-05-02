105045e09448f7f6c234bfe8072d552e101a6076
Barukimang <barukimang@gmail.com>
2024-05-02 14:21:22 +0300

    fix dao creation

--- e2e/tests/token-voting-plugin/new-token/01_createDAO.spec.ts
+++ e2e/tests/token-voting-plugin/new-token/01_createDAO.spec.ts
@@ -75,7 +75,7 @@ test('Create Token Based DAO (new token)', async ({
   await metamask.approveSwitchNetwork();
   await page.waitForTimeout(1000);
   await page.getByRole('button', {name: 'Deploy your DAO'}).click();
-  await page.getByRole('button', {name: 'Approve transaction'}).click();
+  await page.getByRole('button', {name: 'Deploy DAO now'}).click();
   await metamask.confirmTransaction();
   await page.getByRole('button', {name: 'Launch DAO Dashboard'}).click();
   await page.getByRole('button', {name: 'Open your DAO'}).click();
