1e8135f6a19ccd2acb3aec6bd7ad83ef4af4fd15
sepehr sanaee <sepehr2mail@gmail.com>
2024-06-25 22:59:47 +0330

    update creation dao blockchain sorting

--- src/containers/selectChainForm/index.tsx
+++ src/containers/selectChainForm/index.tsx
@@ -105,9 +105,9 @@ type SelectableNetworks = Record<
 
 const networks: SelectableNetworks = {
   main: {
-    cost: ['polygon', 'zksync', 'base', 'arbitrum', 'ethereum'],
-    popularity: ['ethereum', 'zksync', 'polygon', 'arbitrum', 'base'],
-    security: ['ethereum', 'zksync', 'base', 'arbitrum', 'polygon'],
+    cost: ['polygon', 'base', 'arbitrum', 'ethereum', 'zksync'],
+    popularity: ['ethereum', 'arbitrum', 'base', 'zksync'],
+    security: ['ethereum', 'base', 'arbitrum', 'polygon', 'zksync'],
   },
   test: {
     cost: ['sepolia', 'zksyncSepolia'],
