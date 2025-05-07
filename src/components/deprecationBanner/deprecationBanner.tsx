8ee4e9eddd4456c2121fe8c4d84f2f4a72628c35
Kevin Davis <whomst@kevind.xyz>
2025-05-07 18:34:30 +0200

    chore: lint

--- src/components/deprecationBanner/deprecationBanner.tsx
+++ src/components/deprecationBanner/deprecationBanner.tsx
@@ -1,5 +1,4 @@
 import {Button, IconType} from '@aragon/ods';
-import {copyFile} from 'fs';
 import React from 'react';
 import {useTranslation} from 'react-i18next';
 import styled from 'styled-components';
